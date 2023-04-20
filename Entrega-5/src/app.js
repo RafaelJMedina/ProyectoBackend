133816140544ded85b7a29b0e9f9e7d1a4790219
Rafael Medina <rafaelmedina350@hotmail.com>
2023-04-19 21:56:55 -0300

    Entrega-5
    Configurando server con handlebars y socket.io

---
+++ Entrega-5/src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import handlerbars from "express-handlebars";
+import { Server } from 'socket.io';
+ 
+import __dirname from  './utils.js';
+import viewRouter from './routes/views.routes.js';
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(__dirname+'/public')); //Sirve para guardar los js para el front
+
+app.engine('handlebars', handlerbars.engine()); //levanta handlerbars
+app.set('views', __dirname+'/views');
+app.set('view engine', 'handlebars');
+
+app.use('/', viewRouter);
+
+const server = app.listen(PORT, ()=>{
+    console.log('Servidor funcionando en el puerto: ' + PORT)
+})
+
+const socketServerIO = new Server(server);
+
+socketServerIO.on('connection', socket=>{
+
+    console.log('Usuario conectado');
+
+    socket.on("message", data => {
+        socketServerIO.emit('log', data);
+    })
+})
